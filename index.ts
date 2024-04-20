687c235e1758707fb62493fa2fb690bec1068fd5
simon3000 <simon30002021@icloud.com>
2024-04-20 15:28:17 +0200

    export wav

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { join } from 'path'
+
+import { importPCK, unpack, exportFiles } from './hoyo-voice-extractor/index.js'
+
+const PCK_SOURCE = join(import.meta.dirname, 'AudioAssets')
+const RESULT = join(import.meta.dirname, 'result')
+
+await importPCK(PCK_SOURCE)
+
+await unpack()
+
+await exportFiles(RESULT)
